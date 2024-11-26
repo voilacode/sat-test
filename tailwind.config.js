09851a8d03bdd64bb2ae32d1dc0166e6368a50d5
anupam.j <anupam.j@apcfss.in>
2024-11-26 18:52:08 +0530

    changes in instructions and breaktime

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        spin: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+      animation: {
+        spin: 'spin 1s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
